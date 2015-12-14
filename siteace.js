8752d035479274e24e5f84b21a178458a9c3c01b
Sup-Dev <sourabh.dev90@gmail.com>
2015-12-14 07:32:59 +0530

    feat: added login

--- siteace.js
+++ siteace.js
@@ -2,6 +2,10 @@ Websites = new Mongo.Collection("websites");
 
 if (Meteor.isClient) {
 
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_AND_EMAIL"
+	});
+
 	/////
 	// template helpers 
 	/////
